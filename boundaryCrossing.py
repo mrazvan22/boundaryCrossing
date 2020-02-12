ff4ca85ec2d8451d78ac1757e5ab11de7324a6cd
Razvan Marinescu <mrazvan22@gmail.com>
2020-02-12 18:59:37 -0500

    added python file

---
+++ boundaryCrossing.py
@@ -0,0 +1,121 @@
+import os
+import torch
+import matplotlib.pyplot as pl
+import pylab
+import numpy as np
+
+from pytorch_pretrained_biggan.pytorch_pretrained_biggan import (BigGAN, one_hot_from_names, truncated_noise_sample,
+                                       save_as_images, display_in_terminal)
+
+# load the MNIST models
+from gan_pretrained_pytorch.mnist_dcgan.dcgan import Discriminator, Generator
+
+# OPTIONAL: if you want to have more information on what's happening, activate the logger as follows
+import logging
+logging.basicConfig(level=logging.INFO)
+
+import nltk
+nltk.download('wordnet')
+
+# Load pre-trained model tokenizer (vocabulary)
+model = BigGAN.from_pretrained('biggan-deep-256')
+
+# Prepare a input
+truncation = 0.4
+class_vector = one_hot_from_names(['soap bubble', 'coffee', 'mushroom'], batch_size=3)
+noise_vector = truncated_noise_sample(truncation=truncation, batch_size=3)
+
+# All in tensors
+noise_vector = torch.from_numpy(noise_vector)
+class_vector = torch.from_numpy(class_vector)
+
+# If you have a GPU, put everything on cuda
+noise_vector = noise_vector.to('cuda')
+class_vector = class_vector.to('cuda')
+model.to('cuda')
+
+# Generate an image
+with torch.no_grad():
+    output = model(noise_vector, class_vector, truncation)
+
+# If you have a GPU put back on CPU
+output = output.to('cpu')
+
+# If you have a sixtel compatible terminal you can display the images in the terminal
+# (see https://github.com/saitoha/libsixel for details)
+display_in_terminal(output)
+
+# Save results as png images
+save_as_images(output)
+
+
+
+
+
+
+asd
+
+debug = False
+if not debug:
+    num_gpu = 1 if torch.cuda.is_available() else 0
+
+
+
+    D = Discriminator(ngpu=1).eval()
+    G = Generator(ngpu=1).eval()
+
+    # load weights
+    D.load_state_dict(torch.load('gan_pretrained_pytorch/mnist_dcgan/weights/netD_epoch_99.pth'))
+    G.load_state_dict(torch.load('gan_pretrained_pytorch/mnist_dcgan/weights/netG_epoch_99.pth'))
+    if torch.cuda.is_available():
+        D = D.cuda()
+        G = G.cuda()
+
+######## generate some images #########
+
+batch_size = 1
+latent_size = 100
+
+fixed_noise = torch.randn(1, latent_size, 1, 1)
+print('fixed_noise', fixed_noise.shape)
+nrImg = 10
+latent_prog = torch.tensor(np.zeros((nrImg, latent_size, 1, 1), np.double), dtype=torch.float)
+# import pdb
+# pdb.set_trace()
+latent_prog[0,:,:,:] = fixed_noise
+for i in range(1,nrImg):
+    latent_prog[i, :, :, :] = fixed_noise
+    latent_prog[i, 0, :, :] = 10 * np.double(i)/nrImg
+
+
+print('latent_prog[0,:,:,:]', latent_prog[0,:,:,:])
+print('latent_prog[1,:,:,:]', latent_prog[1,:,:,:])
+print('size latent_prog', latent_prog.shape)
+
+if torch.cuda.is_available():
+    latent_prog = latent_prog.cuda()
+
+fake_images = G(latent_prog)
+
+
+# z = torch.randn(1, latent_size).cuda()
+# z = Variable(z)
+# fake_images = G(z)
+
+fake_images_np = fake_images.cpu().detach().numpy()
+fake_images_np = fake_images_np.reshape(fake_images_np.shape[0], 28, 28)
+R, C = 5, 5
+for i in range(nrImg):
+    pl.subplot(R, C, i + 1)
+    pl.imshow(fake_images_np[i], cmap='gray')
+
+fig = pl.gcf()
+fig.show()
+
+
+######## run discriminator #########
+
+outputs = D(fake_images)
+print(outputs)
+
+pl.pause(1000)
